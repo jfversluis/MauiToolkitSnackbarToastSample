1c50f9cbfebcffff4c5c1b6dd71a17af5b6aac4d
Gerald Versluis <gerald.versluis@microsoft.com>
2022-11-29 14:04:15 +0100

    Implement Toast & Snackbar

--- MauiToolkitSnackbarToastSample/MainPage.xaml.cs
+++ MauiToolkitSnackbarToastSample/MainPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿namespace MauiToolkitSnackbarToastSample;
+﻿using CommunityToolkit.Maui.Alerts;
+
+namespace MauiToolkitSnackbarToastSample;
 
 public partial class MainPage : ContentPage
 {
@@ -12,13 +14,28 @@ public partial class MainPage : ContentPage
 	private void OnCounterClicked(object sender, EventArgs e)
 	{
 		count++;
+		string message;
 
 		if (count == 1)
-			CounterBtn.Text = $"Clicked {count} time";
+            message = $"Clicked {count} time";
 		else
-			CounterBtn.Text = $"Clicked {count} times";
+            message = $"Clicked {count} times";
 
-		SemanticScreenReader.Announce(CounterBtn.Text);
+		SemanticScreenReader.Announce(message);
+
+		CounterBtn.Text = message;
+
+		//var toast = Toast.Make(message, CommunityToolkit.Maui.Core.ToastDuration.Long, 30);
+		//toast.Show();
+
+		var snackbar = Snackbar.Make(message, () => DisplayAlert("Did you subscribe", "to my channel yet?", "OK"), "YES!",
+			TimeSpan.FromSeconds(10), new CommunityToolkit.Maui.Core.SnackbarOptions
+			{
+				BackgroundColor = Colors.Red,
+				TextColor = Colors.White,
+			}, dotnetbotimage);
+
+		snackbar.Show();
 	}
 }
 
